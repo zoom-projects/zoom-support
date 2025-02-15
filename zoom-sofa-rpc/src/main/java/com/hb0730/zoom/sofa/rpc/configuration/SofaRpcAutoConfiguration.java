9ece15c5111389b5835c067ec01802e8793bc323
hb0730 <1278032416@qq.com>
2025-02-15 21:34:44 +0800

    chore: 删除无用代码

--- zoom-sofa-rpc/src/main/java/com/hb0730/zoom/sofa/rpc/configuration/SofaRpcAutoConfiguration.java
+++ zoom-sofa-rpc/src/main/java/com/hb0730/zoom/sofa/rpc/configuration/SofaRpcAutoConfiguration.java
@@ -1,8 +1,5 @@
 package com.hb0730.zoom.sofa.rpc.configuration;
 
-import com.hb0730.zoom.base.AppUtil;
-import com.hb0730.zoom.base.utils.StrUtil;
-import com.hb0730.zoom.sofa.rpc.core.config.ConfigManager;
 import com.hb0730.zoom.sofa.rpc.core.factory.SofaRpcBeanRegistryPostProcessor;
 import com.hb0730.zoom.sofa.rpc.core.factory.SofaRpcServiceInitializingBean;
 import lombok.extern.slf4j.Slf4j;
@@ -24,25 +21,5 @@ public class SofaRpcAutoConfiguration {
     @Bean
     public SofaRpcBeanRegistryPostProcessor sofaRpcBeanRegister() {
         return new SofaRpcBeanRegistryPostProcessor();
-    }
-
-
-    private void intRpcRouter() {
-        String env = AppUtil.getActiveProfile();
-
-        String filename = String.format("zoom-app-%s.properties", StrUtil.isBlank(env) ? "default" : env);
-        try {
-            try {
-
-                log.info("加载RPC配置文件: {}", filename);
-                ConfigManager.getInstance().loadProperties(filename);
-            } catch (Exception e) {
-                log.warn("加载RPC配置文件[{}]失败, 加载默认配置: zoom-app.properties", filename);
-                ConfigManager.getInstance().loadProperties("zoom-app.properties");
-            }
-
-        } catch (Throwable e) {
-            log.error("!!!加载RPC配置文件失败!!!", e);
-        }
     }
 }
