a7137506b185b5bc4ae1274b8f82174b2beeaa8d
hb0730 <1278032416@qq.com>
2025-03-30 12:11:26 +0800

    feat: add certificate compression and upload functionality with improved error logging

---
+++ zoom-base/src/main/java/com/hb0730/zoom/base/utils/CompressUtil.java
@@ -0,0 +1,10 @@
+package com.hb0730.zoom.base.utils;
+
+/**
+ * 压缩工具类
+ *
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2025/3/29
+ */
+public class CompressUtil extends cn.hutool.extra.compress.CompressUtil {
+}
